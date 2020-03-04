74b77797dd86dbb8c6673598e379cdbb7369a6da
Zachary <zcy621799@163.com>
2020-03-04 18:40:44 +0800

    prepare

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+from tools.mots_tools.io import *
 import datetime
 import math
 import os
@@ -16,6 +16,9 @@ import matplotlib.pyplot as plt
 import torchvision
 from roi_align import RoIAlign
 from roi_align import CropAndResize
+
+def format_box(bbox):
+    return torch.Tensor([[bbox[0], bbox[1], bbox[0]+ bbox[2], bbox[1] + bbox[3]]])
 
 class SamePad2d(nn.Module):
     """Mimics tensorflow's 'SAME' padding.
@@ -141,56 +144,43 @@ if __name__=='__main__':
     device = torch.device("cuda:"+str(0) if torch.cuda.is_available() else "cpu")
     resnet = ResNet("resnet101", stage5=True)
     C1, C2, C3, C4, C5 = resnet.stages()
-    img = cv2.imread('000001.jpg')
-    print(img.shape)
-    img = img[:,:,:].transpose(2, 0, 1)
-    img = np.ascontiguousarray(img, dtype=np.float32)
-    img /= 255.0
-    img = img[None]
-    print(img.shape)
-    torchimg = torch.from_numpy(img)
 
-    resnet = resnet.cuda()
-    torchimg=torchimg.cuda()
-    out = C1(torchimg)
-    print(out.shape)
-    out = C2(out)
-    print(out.shape)
-    out = C3(out)
-    print(out.shape)
-    out = C4(out)
-    print(out.shape)
-    data=out.cpu().detach()
+    filePath = r"E:\Challenge\MOTSChallenge\train\instances_txt"
 
-    # for example, we have two bboxes with coords xyxy (first with batch_id=0, second with batch_id=1).
-    boxes = torch.Tensor([[10/1080*34, 20/1920*60, 540/1080*34, 640/1920*60]]).cuda()
-    #做好坐标比例变化
+    filename = os.path.join(filePath, "{:04}.txt".format(2))
+
+    txt = load_txt(filename)
+    for obj in txt[1]:
+        mask = rletools.decode(obj.mask)
+        mask=torch.from_numpy(mask)
+        mask=mask.float()
+        mask=mask[None]
+        mask = mask[None]
+        mask = mask.contiguous()
+        boxes = format_box(rletools.toBbox(obj.mask))
+        # 做好坐标比例变化
+
+        box_index = torch.tensor([0], dtype=torch.int)  # index of bbox in batch
+
+        # RoIAlign layer with crop sizes:
+        crop_height = 388
+        crop_width = 164
+        roi_align = RoIAlign(crop_height, crop_width, 0.25)
+
+        # make crops:
+        crops = roi_align(mask, boxes, box_index)  # 输入必须是tensor，不能是numpy
+
+        # plt.imshow(img[0][0])
+        # plt.show()
+        plt.imshow(crops[0][0])
+        print(crops[0][0])
+        plt.show()
+        break
 
 
-    box_index = torch.tensor([0, 1], dtype=torch.int).cuda()  # index of bbox in batch
 
-    # RoIAlign layer with crop sizes:
-    crop_height = 28
-    crop_width = 14
-    roi_align = RoIAlign(crop_height, crop_width,0.25)
 
-    # make crops:
-    crops = roi_align(out.cuda(), boxes, box_index) #输入必须是tensor，不能是numpy
-    print(type(crops))
-    net = torch.nn.ConvTranspose2d(1024, 256, (2, 2), stride=2, padding=2).cuda()
-    net1 = torch.nn.ConvTranspose2d(256, 64, (2, 2), stride=2, padding=2).cuda()
-    net2 = torch.nn.ConvTranspose2d(64, 16, (2, 2), stride=2, padding=2).cuda()
-    net3 = torch.nn.ConvTranspose2d(16, 1, (2, 2), stride=2, padding=2).cuda()
-    crops = net(crops)
-    crops = net1(crops)
-    crops = net2(crops)
-    crops = net3(crops)
 
-    # plt.imshow(img[0][0])
-    # plt.show()
-    print(crops.shape)
-    crops=crops.cpu().detach().numpy()
-    plt.imshow(crops[0][0])
-    plt.show()
 
-    # ConvTranspose2d(256, 256, kernel_size=(2, 2), stride=(2, 2)
+
+
