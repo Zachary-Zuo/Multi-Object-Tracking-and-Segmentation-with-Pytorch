8fa60d25cd01880fa65affe467df261f9d1d26ca
Zachary <zcy621799@163.com>
2020-03-17 15:38:20 +0800

    update seghead

--- network/seghead.py
+++ network/seghead.py
@@ -7,6 +7,34 @@ from network.backbone import ResNet
 from roi_align import RoIAlign
 from roi_align import CropAndResize
 
+def make_conv3x3(
+    in_channels,
+    out_channels,
+    dilation=1,
+    stride=1,
+    use_gn=False,
+    kaiming_init=True
+):
+    conv = nn.Conv2d(
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        dilation=dilation,
+        bias=False if use_gn else True
+    )
+    if kaiming_init:
+        nn.init.kaiming_normal_(
+            conv.weight, mode="fan_out", nonlinearity="relu"
+        )
+    else:
+        torch.nn.init.normal_(conv.weight, std=0.01)
+    if not use_gn:
+        nn.init.constant_(conv.bias, 0)
+
+    return conv
+
 class SegHead(nn.Module):
 
     def __init__(self,img_size):
@@ -16,39 +44,69 @@ class SegHead(nn.Module):
         self.feature_height=math.ceil(self.img_height/16)
         self.feature_width = math.ceil(self.img_width / 16)
 
-        crop_height = 28
-        crop_width = 14
-        self.roi_align = RoIAlign(crop_height, crop_width, 0.25)
+        self.crop_height = 28
+        self.crop_width = 14
 
         self.box_index = torch.tensor([0], dtype=torch.int).cuda()
 
-        self.seg = nn.Sequential(
-            torch.nn.ConvTranspose2d(1024, 256, (2, 2), stride=2, padding=2),
-            torch.nn.ConvTranspose2d(256, 64, (2, 2), stride=2, padding=2),
-            torch.nn.ConvTranspose2d(64, 16, (2, 2), stride=2, padding=2),
-            torch.nn.Conv2d(16, 1, (1, 1))
+        self.blocks = []
+        for id in range(4):
+            layer_name = "mask_fcn{}".format(id)
+            module = make_conv3x3(256, 256,dilation=1, stride=1)
+            self.add_module(layer_name, module)
+            self.blocks.append(layer_name)
+
+        self.zoomboxes = torch.Tensor([[0, 0, 28, 56]]).cuda()
+
+        self.conv5_mask = torch.nn.ConvTranspose2d(256, 256, 2, 2, 0)
+        self.mask_fcn_logits = torch.nn.Conv2d(256, 1, 1, 1, 0)
+
+        nn.init.kaiming_normal_(
+            self.conv5_mask.weight, mode="fan_out", nonlinearity="relu"
         )
+        nn.init.constant_(self.conv5_mask.bias, 0)
 
-        self.zoomboxes = torch.Tensor([[0, 0, 84, 196]]).cuda()
+        nn.init.kaiming_normal_(
+            self.mask_fcn_logits.weight, mode="fan_out", nonlinearity="relu"
+        )
+        nn.init.constant_(self.mask_fcn_logits.bias, 0)
 
+        # for name, param in self.named_parameters():
+        #     if "bias" in name:
+        #         nn.init.constant_(param, 0)
+        #     elif "weight" in name:
+        #         nn.init.kaiming_normal_(param, mode="fan_out", nonlinearity="relu")
+
+    def pooler(self,fms,bbox):
+        sampling_ratio = 0.03125
+        for i, fm in enumerate(reversed(fms)):
+            fw, fh = fm.shape[-2:]
+            if bbox[0]/self.img_height*fh > 6 and bbox[1]/self.img_width*fw>3:
+                roi_align = RoIAlign(self.crop_height, self.crop_width, sampling_ratio)
+                boxes = self.format_box(bbox, fw, fh).cuda()
+                crops = roi_align(fm, boxes, self.box_index)  # 输入必须是tensor，不能是numpy
+                return crops
+            sampling_ratio *= 2
+        fm = fms[0]
+        fw, fh = fm.shape[-2:]
+        roi_align = RoIAlign(self.crop_height, self.crop_width, sampling_ratio/2)
+        boxes = self.format_box(bbox, fw, fh).cuda()
+        crops = roi_align(fm, boxes, self.box_index)  # 输入必须是tensor，不能是numpy
+        return crops
 
 
     def forward(self,fms,bbox_list):
         x=[]
         for bbox in bbox_list:
-            out = []
-            for fm in fms:
-                fw,fh = fm.shape[-2:]
-                bbox = bbox.squeeze()
-                boxes = self.format_box(bbox,fw,fh).cuda()
-                crops = self.roi_align(fm, boxes, self.box_index)  # 输入必须是tensor，不能是numpy
-                out.append(crops)
-            output = torch.cat(out,dim=1)
-            crops=self.seg(output)
+            bbox = bbox.squeeze()
+            out = self.pooler(fms,bbox)
+            for layer_name in self.blocks:
+                out = F.relu(getattr(self, layer_name)(out))
+            out = self.conv5_mask(out)
+            out = self.mask_fcn_logits(out)
             zoom_roi_align = RoIAlign(bbox[3], bbox[2], 0.25)
-            crops = zoom_roi_align(crops, self.zoomboxes, self.box_index)
-
-            x.append(crops.squeeze())
+            out = zoom_roi_align(out, self.zoomboxes, self.box_index)
+            x.append(out.squeeze())
         return x
 
 
@@ -61,18 +119,17 @@ class SegHead(nn.Module):
 
 if __name__=='__main__':
     net = SegHead((1920,1080))
-    for ml1 in net.modules():
-        if isinstance(ml1, nn.Sequential):
-            for ml2 in ml1:
-                if isinstance(ml2, nn.ConvTranspose2d):
-                    nn.init.kaiming_normal_(ml2.weight.data)
-                elif isinstance(ml2, nn.Conv2d):
-                    nn.init.kaiming_normal_(ml2.weight.data)
-        elif isinstance(ml1, nn.ConvTranspose2d):
-            nn.init.kaiming_normal_(ml1.weight.data)
-        elif isinstance(ml1, nn.Conv2d):
-            nn.init.kaiming_normal_(ml1.weight.data)
-        else:
-            print(type(ml1))
+    # for ml1 in net.modules():
+    #     if isinstance(ml1, nn.Sequential):
+    #         for ml2 in ml1:
+    #             if isinstance(ml2, nn.ConvTranspose2d):
+    #                 nn.init.kaiming_normal_(ml2.weight.data,mode="fan_out", nonlinearity="relu")
+    #             elif isinstance(ml2, nn.Conv2d):
+    #                 nn.init.kaiming_normal_(ml2.weight.data)
+    #     elif isinstance(ml1, nn.ConvTranspose2d):
+    #         nn.init.kaiming_normal_(ml1.weight.data)
+    #     elif isinstance(ml1, nn.Conv2d):
+    #         nn.init.kaiming_normal_(ml1.weight.data)
+    #     else:
+    #         print(type(ml1))
     torch.save(net.state_dict(), 'seg.pth')
-
